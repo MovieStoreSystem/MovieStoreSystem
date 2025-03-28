2e311bb1403b81e5681bd41ccb06c3f65606bda3
omran18 <saramahmoudomran@gmail.com>
2025-03-28 02:46:06 +0200

    #5tth

--- SPproject/main.cpp
+++ SPproject/main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 int main() {
+
 	int x, y;
 	cout << "hi sara, I am sara version 2 \n";
 	cout << "the sum is: " << x + y << "\n";
+	cout << "the division is: " << x / y<<"\n";
+	cout << "happy eid 2025\n";
 	return 0;
 }

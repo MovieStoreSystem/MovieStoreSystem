11fd7863b8be21b6d105c3f4862ae0b36967ac13
saraomran <so2767288@gmail.com>
2025-03-28 06:29:35 +0200

    #againnnn

--- SPproject/master.cpp
+++ SPproject/master.cpp
@@ -1,17 +1,49 @@
 #include <iostream>
-using namespace  std;
-char choice;
+using namespace std;
+char menu();
 
-// menu;
-void menu() {
-	cout << "1.rent a movie \n2.exit";
-	cin >> choice;
-}
 
 int main() {
-	menu();
-	cout << "it worked";
-	cout << "ITS ME JANNAH. CAN YOU SEE ME.";
-	cout << "COMMIT PLEASE";
+	switch (menu())
+	{
+	case' 1':
+		//
+		break;
+
+	case '2':
+		//
+		break;
+
+	case '3':
+		//
+		break;
+
+	case '4':
+		//
+		break;
+
+	case '5':
+		//
+		break;
+
+	default:
+		cout << "Sorry, Invalid choice!";
+		break;
+	}
 	return 0;
+}
+
+//menu of the system!
+char menu()
+{
+	char choice;
+	cout << "\t\tWelcome to our Movie Store\n";
+	cout << "1.View Movies\n";
+	cout << "2.Rent a Movie\n";
+	cout << "3.Return a Movie\n";
+	cout << "4.Rental History\n";
+	cout << "5.Exit\n";
+	cout << "Please enter your choice number: ";
+	cin >> choice;
+	return choice;
 }

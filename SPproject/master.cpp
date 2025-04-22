6ac3d4e09e04fc720db8b783690c3af90caf3325
saraomran <so2767288@gmail.com>
2025-04-22 22:14:34 +0200

    #after adding sign in/up ;)

--- SPproject/master.cpp
+++ SPproject/master.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include<string>
 using namespace std;
 
 //structs and variables
@@ -6,7 +7,7 @@ struct accountinfo
 {
 	string username;
 	string email;
-	char password[20];
+	string password;
 	int movienumber;
 	int accountnumber;
 };
@@ -22,40 +23,64 @@ struct user
 
 //functions
 char menu();
-bool findemail(string email, user users[], int totalUsers);
-bool signup(user& temp, user users[], int totalUsers);
+bool findemail(string email, user users[], int& totalusers);
+bool findphonenumber(string phonenum, user users[], int& totalusers);
+bool findaccountnum(int accountnum, user users[], int& totalusers);
+bool findtrueinfo(string email, string password, user users[], int& totalusers);
+bool signin(user& temp, user users[], int& totalusers);
+bool signup(user& temp, user users[], int& totalusers);
 
 
 int main() {
 	int totalusers = 0;
+	char ch = 'y', choice;
 	user users[20];
-	switch (menu())
+	cout << "1.Sign up \n2.Sign in\n";
+	cin >> choice;
+	switch (choice)
 	{
-	case' 1':
-		//
-		break;
-
-	case '2':
+	case'1':
 		signup(users[totalusers], users, totalusers);
 		totalusers++;
 		break;
-
-	case '3':
-		//
+	case'2':
+		signin(users[totalusers], users, totalusers);
 		break;
-
-	case '4':
-		//
-		break;
-
-	case '5':
-		//
-		break;
-
 	default:
-		cout << "Sorry, Invalid choice!";
+		cout << "invalid choice";
 		break;
 	}
+	do {
+
+		switch (menu())
+		{
+		case'1':
+			//
+			break;
+
+		case '2':
+			//
+			break;
+
+		case '3':
+			//
+			break;
+
+		case '4':
+			//
+			break;
+
+		case '5':
+			//
+			break;
+
+		default:
+			cout << "Sorry, Invalid choice!";
+			break;
+		}
+		cout << "Do you want to continue(y/n)\n";
+		cin >> ch;
+	} while (ch == 'y' || ch == 'Y');
 	return 0;
 }
 
@@ -74,9 +99,9 @@ char menu()
 	return choice;
 }
 
-bool findemail(string email, user users[], int totalUsers)
+bool findemail(string email, user users[], int& totalusers)
 {
-	for (int i = 0; i < totalUsers; i++) {
+	for (int i = 0; i < totalusers; i++) {
 		if (email == users[i].useraccount.email) {
 			return true;
 		}
@@ -84,10 +109,42 @@ bool findemail(string email, user users[], int totalUsers)
 	return false;
 }
 
-bool signup(user& temp, user users[], int totalUsers)
+bool findphonenumber(string phonenum, user users[], int& totalusers) {
+	{
+		for (int i = 0; i < totalusers; i++) {
+			if (phonenum == users[i].phonenumber) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+bool findaccountnum(int accountnum, user users[], int& totalusers) {
+	{
+		for (int i = 0; i < totalusers; i++) {
+			if (accountnum == users[i].useraccount.accountnumber) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+bool findtrueinfo(string email, string password, user users[], int& totalusers) {
+	for (int i = 0;i < totalusers;i++) {
+		if (email == users[i].useraccount.email && password == users[i].useraccount.password)
+			return true;
+	}
+	return false;
+}
+
+
+bool signup(user& temp, user users[], int& totalusers)
 {
 	cout << "Enter your username:\n";
-	cin >> temp.useraccount.username;
+	cin.ignore();
+	getline(cin, temp.useraccount.username);
 
 	cout << "Enter your age:\n";
 	cin >> temp.age;
@@ -99,11 +156,15 @@ bool signup(user& temp, user users[], int totalUsers)
 
 	cout << "Enter your phone number:\n";
 	cin >> temp.phonenumber;
+	if (findphonenumber(temp.phonenumber, users, totalusers)) {
+		cout << "phone number already used" << endl;
+		return false;
+	}
 
 	cout << "Enter your email:\n";
 	cin >> temp.useraccount.email;
 
-	if (findemail(temp.useraccount.email, users, totalUsers)) {
+	if (findemail(temp.useraccount.email, users, totalusers)) {
 		cout << "This email already exists!\n";
 		return false; // ✅ Sign-up failed
 	}
@@ -111,5 +172,24 @@ bool signup(user& temp, user users[], int totalUsers)
 	cout << "Enter your password:\n";
 	cin >> temp.useraccount.password;
 
-	return true; // ✅ Sign-up succeeded
+	temp.useraccount.accountnumber = (rand() % 100) + 5;
+	while (findaccountnum(temp.useraccount.accountnumber, users, totalusers)) {
+		temp.useraccount.accountnumber = (rand() % 100) + 5;
+	}
+	return true;  // ✅ Sign-up succeeded
+}
+
+bool signin(user& temp, user users[], int& totalusers) {
+	cout << "Enter your email" << endl;
+	cin >> temp.useraccount.email;
+	cout << "Enter your password" << endl;
+	cin >> temp.useraccount.password;
+	if (findtrueinfo(temp.useraccount.email, temp.useraccount.password, users, totalusers)) {
+		cout << "loged in successfully\n";
+		return true;
+	}
+	else {
+		cout << "this account doesn't exist\n";
+		return false;
+	}
 }

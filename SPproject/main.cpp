82e1169cc7352808c2becd25070b3f5aea33a01a
menna293 <rehabemad23775@gmail.com>
2025-04-25 18:19:44 +0300

    دي اخر حاجة ان شاء الله

--- SPproject/main.cpp
+++ SPproject/main.cpp
@@ -1,52 +1,35 @@
 #include <iostream>
 using namespace std;
-struct movies {
-	string name[100];
-	char avlb;
-	int count;
-};
-void rentcount(int count,string m[])
-{
-	for (int j = 0;j < count; j++)
-	{
-		movies mv;
-		count = 0;
-		if (m[j] == mv.name[j])
-			count++;
-
-	}
-}
-void name(int count, string m[])
+struct movieinfo {
+	string name_of_movie;
+	float price = 0;
+	int rentingCount = 0;
+	int Quantity;
+	float average_rate = 0;
+	int final_score_of_movie = 0;
+	int total_ratings = 0;
+}movie;
+void name(int count  )
 {
 	cout << "Enter the movie name" << '\n';
 	for (int i = 0;i < count;i++)
-		cin >> m[i];
+		cin >> movie.name_of_movie[i];
 
 }
-void av(int count ,char avlb, string m[])
+void addcount(int count)
 {
-	movies v;
 	for (int i = 0;i < count;i++)
 	{
-		if (m[i] == v.name[i])
-			cout << "This movie is available in our websit" << '\n';
-		else
-			cout << "this movie is unavailable in our websit" << '\n';
+		cout << "Enter the count of the movie" << '\n';
+		cin >> movie.Quantity;
 	}
-	
 }
-void moviemanagment(string m[], char avlb, int count)
+void moviemanagment(int count)
 {
-	movies m;
-	string names[100];
-	char avlb;
-	int count = 0;
-	cout << "Enter the count of movies you want to rent" << '\n';
+	cout << "Enter the count of movies you want to input" << '\n';
 	cin >> count;
-	name(count, names);
-	av(count, avlb, names);
-	rentcount(count, names);
-
+	name(count);
+	addcount(count);
 }
 void main()
 {

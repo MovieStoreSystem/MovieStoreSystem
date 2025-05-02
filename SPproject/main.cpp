ad3943d5cc898dc976d610f971584f0249655e6b
menna293 <rehabemad23775@gmail.com>
2025-05-02 19:16:08 +0300

    كده تمام

--- SPproject/main.cpp
+++ SPproject/main.cpp
@@ -1,37 +1,64 @@
 #include <iostream>
+#include <string>
 using namespace std;
+const int number_of_movies = 20;
+int totalnumofmovies = 0;
 struct movieinfo {
 	string name_of_movie;
-	float price = 0;
+	float price = 0,overdue_price = 0.0;
 	int rentingCount = 0;
-	int Quantity;
+	int Quantity =0;
 	float average_rate = 0;
 	int final_score_of_movie = 0;
 	int total_ratings = 0;
-}movie;
-void name(int count  )
+}movie[number_of_movies];
+void addingMovies(int count, movieinfo movie[])
 {
-	cout << "Enter the movie name" << '\n';
-	for (int i = 0;i < count;i++)
-		cin >> movie.name_of_movie[i];
 
-}
-void addcount(int count)
-{
-	for (int i = 0;i < count;i++)
-	{
-		cout << "Enter the count of the movie" << '\n';
-		cin >> movie.Quantity;
+	if (totalnumofmovies > number_of_movies) {
+		cout << "sorry we have reached the maximum number of movies\n";
+		return;
+	}
+	else {
+		for (int i = totalnumofmovies;i < count + totalnumofmovies;i++) {
+			cout << "Enter the movie name:\n";
+			cin.ignore();
+		    getline(cin, movie[i].name_of_movie);
+			cout << "Enter the price of the movie for a day:\n";
+			cin >> movie[i].price;
+			cout << "Enter the quantity of the movie\n";
+			cin >> movie[i].Quantity;
+
+			if (i > number_of_movies) {
+				cout << "sorry we have reached the maximum number of movies\n";
+				return;
+			}
+		}
+		totalnumofmovies += count;
 	}
 }
-void moviemanagment(int count)
+void moviemanagment()
 {
-	cout << "Enter the count of movies you want to input" << '\n';
-	cin >> count;
-	name(count);
-	addcount(count);
+	int count;
+	while (true) {
+		cout << "Enter how many movies you want to input\n";
+		cin >> count;
+
+		if (cin.fail())
+		{
+			cout << "Please enter a number.\n";
+			cin.clear();
+			cin.ignore(10000, '\n');
+			continue;
+		}
+		else
+			break;
+	}
+	addingMovies(count, movie);
 }
 void main()
 {
-	
+	int totalnumofmovies;
+	int number_of_movies;
+	moviemanagment();
 }
